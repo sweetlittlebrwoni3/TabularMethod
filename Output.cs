2de923b3801db204bd0c81cf7cdd4d58063d9258
sweetlittlebrowni3 <m.pouya@nisys.ir>
2021-07-10 12:07:37 +0430

    Added an output method but didn't write it yet. Waiting for TA's response

---
+++ Output.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabularMethod
+{
+    public class Output
+    {
+        public Output(List<string> BinEPIs)
+        {
+            foreach(string item in BinEPIs)
+            {
+
+            }
+        }
+    }
+}
